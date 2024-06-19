f2a76b3dfe03e9e3ad26b1a2b8e09f77905f2a8e
unknown <alixatofer07@gmail.com>
2024-06-19 20:45:59 +0700

    fix cookie

--- frontend/src/api/baseApi.js
+++ frontend/src/api/baseApi.js
@@ -4,5 +4,20 @@ const api = axios.create({
   baseURL: "https://api-msib-6-forum-diskusi-04.educalab.id",
   withCredentials: true,
 });
-
+api.interceptors.response.use(
+  (response) => {
+    const setCookieHeader = response.headers['set-cookie'];
+    if (setCookieHeader) {
+      const tokenCookie = setCookieHeader.find(cookie => cookie.startsWith('token='));
+      if (tokenCookie) {
+        const tokenValue = tokenCookie.split(';')[0].split('=')[1];
+        localStorage.setItem('token', tokenValue);
+      }
+    }
+    return response;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 export default api;
